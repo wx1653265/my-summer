a8725d0640558401b370f0a9757d14e042ed4ab3
wx1653265 <2639603113@qq.com>
2018-07-28 15:27:58 +0800

    second in evening

---
+++ cat.py
@@ -0,0 +1,9 @@
+cats=input("how many cats?")
+cats=float(cats)
+dogs=30
+if cats<dogs:
+    print"more dogs than cats."
+elif dogs<cats:
+    print"more cats than dogs."
+else:
+    print"same number of cats and dogs."
---
+++ ex14.py
@@ -0,0 +1,21 @@
+a={1,2,3,4}
+for i in a:
+    print i
+    print 1+3
+for i in range(3):
+    print a[i]
+    
+    
+    
+    
+a={1,2,3}
+b={1:'a',2:'b',3:'c'}
+for i in a:
+    print b[i]
+    
+
+
+c={2:'w',3:5}
+for i in c:
+    print i
+    print 0
